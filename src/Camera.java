d0a0acc3a7b5fb2c698ae7cbb1d99bde0a4f21b6
Sean Naegle <sean@naegles.com>
2017-02-04 17:45:59 -0700

    reformatted Camera and Picture to only have tabs of 2 spaces instead of 4 or 6:

--- src/Camera.java
+++ src/Camera.java
@@ -4,66 +4,70 @@ import java.util.HashMap;
 /**
  * Created by Sean on 2/4/17.
  */
-public class Camera implements Sensorable{
-    private static int nextid = 0;
-    private HashMap<Integer, Picture> images = new HashMap<>();
-    private int elapsed_seconds = 0;
-    private Asteroid[] = [];
+public class Camera implements Sensorable {
+  private static int nextid = 0;
+  private HashMap<Integer, Picture> images = new HashMap<>();
+  private int elapsed_seconds = 0;
+  private List<Asteroid> asteroids;
 
-    /**
-     * Initialize a camera object
-     */
-    public Camera() {
-       this.images = new HashMap<Integer, Picture>();
-    }
+  /**
+   * Initialize a camera object
+   */
+  public Camera() {
+    this.images = new HashMap<Integer, Picture>();
+  }
 
-    /**
-     * Returns the status of the Camera.
-     * @return
-     */
-    public int status() {
+  /**
+   * Returns the status of the Camera.
+   *
+   * @return
+   */
+  public int status() {
 
-    }
+  }
 
-    /**
-     * Notifies us to take a picture with a zoom level (1-3) ??
-     * Returns a generated id
-     * @param zoom
-     * @return
-     */
-    public int take_picture(int zoom) {
-        int id = nextid + 1;
-        nextid += 1;
-        Picture picture = generate_image(elapsed_seconds, zoom);
-        images.set(id, picture);
-        return id;
-    }
+  /**
+   * Notifies us to take a picture with a zoom level (1-3) ??
+   * Returns a generated id
+   *
+   * @param zoom
+   * @return
+   */
+  public int take_picture(int zoom) {
+    int id = nextid + 1;
+    nextid += 1;
+    Picture picture = generate_image(elapsed_seconds, zoom);
+    images.set(id, picture);
+    return id;
+  }
 
-    public int take_picture() {
-        return take_picture(0);
-    }
+  public int take_picture() {
+    return take_picture(0);
+  }
 
 
-    /**
-     * Returns a chunk of an image with the given id
-     * @param id
-     * @param x
-     * @param y
-     * @param size
-     * @return
-     */
-    public Image image_chunk(int id, int x, int y, int size) {
-        Picture picture = images.get(id);
-        return picture.chunk(x, y, size);
-    }
+  /**
+   * Returns a chunk of an image with the given id
+   *
+   * @param id
+   * @param x
+   * @param y
+   * @param size
+   * @return
+   */
+  public Image image_chunk(int id, int x, int y, int size) {
+    Picture picture = images.get(id);
+    return picture.chunk(x, y, size);
+  }
 
-    /**
-     * Generate a image at the given time with the given zoom
-     * @param time
-     * @param zoom
-     * @return
-     */
-    private Picture generate_image(int time, int zoom) {
+  /**
+   * Generate a image at the given time with the given zoom
+   *
+   * @param time
+   * @param zoom
+   * @return
+   */
+  private Picture generate_image(int time, int zoom) {
 
-    }
+  }
 }
--- src/Picture.java
+++ src/Picture.java
@@ -4,17 +4,18 @@ import java.awt.*;
  * Created by Sean on 2/4/17.
  */
 public class Picture {
-    private Image image;
+  private Image image;
 
-    /**
-     * returns an chunk of the image where x and y are the center and size is the radius
-     * So it should be a square
-     * @param x
-     * @param y
-     * @param size
-     * @return
-     */
-    public Image chunk(int x, int y, int size) {
+  /**
+   * returns an chunk of the image where x and y are the center and size is the radius
+   * So it should be a square
+   *
+   * @param x
+   * @param y
+   * @param size
+   * @return
+   */
+  public Image chunk(int x, int y, int size) {
 
-    }
+  }
 }
