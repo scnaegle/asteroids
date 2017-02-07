e1a2f3806be8a9758f4db322e199e736361b5eff
Sean Naegle <sean@naegles.com>
2017-02-07 10:33:42 -0700

    did some cleanup and added code for removing oldest picture if we have met our limit. Also remove asteroids that have moved out of our window of sight and generate new asteroids if we are below our Max asteroid limitation

--- src/Camera.java
+++ src/Camera.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 /**
@@ -12,6 +13,8 @@ import java.util.HashMap;
 public class Camera implements Sensorable {
   private static final int[] image_size = {4000,4000};
   private static final int MAX_ASTEROIDS = 5;
+  private static final int MAX_STORED_PICTURES = 5;
+
   private HashMap<Integer, Picture> images = new HashMap<>();
   private int elapsed_seconds = 0;
   private ArrayList<Asteroid> asteroids = new ArrayList<>();
@@ -20,12 +23,11 @@ public class Camera implements Sensorable {
    * Initialize a camera object
    */
   public Camera() {
-    this.images = new HashMap<Integer, Picture>();
+    this.images = new HashMap<>();
   }
 
   /**
    * Returns the status of the Camera.
-   *
    * @return
    */
   public int status() {
@@ -35,14 +37,22 @@ public class Camera implements Sensorable {
   /**
    * Notifies us to take a picture with a zoom level (1-3) ??
    * Returns a generated id
-   *
    * @param zoom
    * @return
    */
   public int take_picture(int zoom) {
+    // Check to make sure we have enough storage space to save another picture. If not, then remove the oldest picture.
+    if (images.size() >= MAX_STORED_PICTURES) {
+      System.out.println("OUT OF MEMORY ERROR: removing oldest picture before taking another...");
+      int oldest_id = Collections.min(images.keySet());
+      images.remove(oldest_id);
+      System.out.format("Removed picture %d\n", oldest_id);
+    }
+
+    // Take a new picture
     Picture picture = generate_image(elapsed_seconds, zoom);
-    images.put(picture.id, picture);
-    return picture.id;
+    images.put(picture.getId(), picture);
+    return picture.getId();
   }
 
   public int take_picture() {
@@ -55,7 +65,6 @@ public class Camera implements Sensorable {
 
   /**
    * Returns a chunk of an image with the given id
-   *
    * @param id
    * @param x
    * @param y
@@ -67,13 +76,17 @@ public class Camera implements Sensorable {
     return picture.chunk(x, y, size);
   }
 
+  /**
+   * Occasionally it may be necessary to get the whole picture object.
+   * @param id
+   * @return
+   */
   public Picture getPicture(int id) {
     return images.get(id);
   }
 
   /**
    * Generate a image at the given time with the given zoom
-   *
    * @param time
    * @param zoom
    * @return
@@ -83,18 +96,58 @@ public class Camera implements Sensorable {
     Graphics2D g = image.createGraphics();
     g.setColor(Color.black);
     g.fillRect(0, 0, image.getWidth(), image.getHeight());
+    System.out.format("Generating picture at time: %d seconds:\n", time);
 
+    // Generate a starting set of asteroids at time = 0
     if (elapsed_seconds == 0) {
       asteroids.add(new Asteroid(new int[]{200, 200, 500}, 500, new int[]{1, 1, -1}));
       generate_random_asteroids();
     }
 
+    // If we no longer have our max number of asteroids then generate some more
+    if (asteroids.size() < MAX_ASTEROIDS) {
+      generate_random_asteroids();
+    }
+
+    // Move all asteroids and draw them on the image
     for (Asteroid asteroid : asteroids) {
       asteroid.move(time);
+      System.out.println(asteroid.toString());
       draw_asteroid_to_image(image, asteroid);
     }
 
+    // Remove all asteroids that will never show up again
+    remove_off_window_asteroids();
+
     return new Picture(image);
+  }
+
+  private void remove_off_window_asteroids() {
+    ArrayList<Asteroid> asteroids_to_be_removed = new ArrayList<>();
+    for (Asteroid asteroid : asteroids) {
+      // If the asteroid is behind us then no additional calculation is required
+      if (asteroid.current_location[2] <= 0) {
+        asteroids_to_be_removed.add(asteroid);
+        //asteroids.remove(asteroid);
+        continue;
+      }
+
+      int xmin = asteroid.current_location[0] - asteroid.current_radius;
+      int xmax = asteroid.current_location[0] + asteroid.current_radius;
+      int ymin = asteroid.current_location[1] - asteroid.current_radius;
+      int ymax = asteroid.current_location[1] + asteroid.current_radius;
+
+      // If the asteroid is currently out of bounds of the image_size then we know it is no longer going to show up.
+      if (xmax < 0 || ymax < 0 || xmin > image_size[0] || ymin > image_size[1]) {
+        //asteroids.remove(asteroid);
+        asteroids_to_be_removed.add(asteroid);
+      }
+    }
+
+    for (Asteroid asteroid : asteroids_to_be_removed) {
+      System.out.format("Removing asteroid %d\n", asteroid.getId());
+      asteroids.remove(asteroid);
+    }
   }
 
   private void generate_random_asteroids() {
@@ -105,17 +158,19 @@ public class Camera implements Sensorable {
 
   private void draw_asteroid_to_image(BufferedImage image, Asteroid asteroid) {
     if (asteroid.current_location[2] <= 0) {
-      System.out.println("Asteroid is now behind us.");
+      System.out.format("Asteroid %d is now behind us.\n", asteroid.getId());
       return;
     }
-    int xmin = Math.max(asteroid.current_location[0] - asteroid.current_radius, 0);
-    int xmax = Math.min(asteroid.current_location[0] + asteroid.current_radius, image.getWidth());
-    int ymin = Math.max(asteroid.current_location[1] + asteroid.current_radius, 0);
-    int ymax = Math.min(asteroid.current_location[1] + asteroid.current_radius, image.getHeight());
+    //int xmin = Math.max(asteroid.current_location[0] - asteroid.current_radius, 0);
+    //int xmax = Math.min(asteroid.current_location[0] + asteroid.current_radius, image.getWidth());
+    //int ymin = Math.max(asteroid.current_location[1] - asteroid.current_radius, 0);
+    //int ymax = Math.min(asteroid.current_location[1] + asteroid.current_radius, image.getHeight());
 
-    Graphics2D g = (Graphics2D) image.createGraphics();
+    Graphics2D g = image.createGraphics();
     g.setColor(Color.gray);
-    g.fillOval(asteroid.current_location[0], asteroid.current_location[1], asteroid.current_radius, asteroid.current_radius);
+    g.fillOval(asteroid.current_location[0] - asteroid.current_radius, asteroid.current_location[1] - asteroid.current_radius, asteroid.current_radius, asteroid.current_radius);
+    g.setColor(Color.green);
+    g.drawString(Integer.toString(asteroid.getId()), asteroid.current_location[0], asteroid.current_location[1]);
   }
 
   public static void main(String[] args) {
@@ -126,7 +181,7 @@ public class Camera implements Sensorable {
       int picture_id = camera.take_picture();
       System.out.format("picture %d was taken\n", picture_id);
       Picture picture = camera.getPicture(picture_id);
-      File output_file = new File("generated_image_" + picture.id + ".png");
+      File output_file = new File("generated_image_" + picture.getId() + ".png");
       try {
         ImageIO.write(picture.get_image(), "png", output_file);
       } catch (IOException e) {
@@ -134,6 +189,5 @@ public class Camera implements Sensorable {
       }
       System.out.format("Output picture %d to generated_image_%d.png\n", picture_id, picture_id);
     }
-
   }
 }
