5afb8b91ee590da5684a59ea8c468bec6d522817
James Holland <jamesfholland@gmail.com>
2017-02-10 22:44:32 -0700

    First stab at GUI demo

---
+++ src/Demo.java
@@ -0,0 +1,36 @@
+/**
+ * Created by jholland on 2/10/17.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Demo extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("DemoGUI.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.sizeToScene();
+    }
+}
