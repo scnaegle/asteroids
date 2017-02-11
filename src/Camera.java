6d482b313895105dd559ceb39d486cd562f9293a
James Holland <jamesfholland@gmail.com>
2017-02-10 22:43:07 -0700

    Remove id portion from Camera and add missing methods

--- src/Camera.java
+++ src/Camera.java
@@ -1,11 +1,6 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 
 /**
  * Created by Sean on 2/4/17.
@@ -16,49 +11,65 @@ public class Camera implements Sensorable {
   private static final int MAX_STORED_PICTURES = 5;
   private static final int TIME_STEP = 30;
 
-  private HashMap<Integer, Picture> images = new HashMap<>();
-  private int image_count = 0;
+  private Picture image;
   private int elapsed_seconds = 0;
   private ArrayList<Asteroid> asteroids = new ArrayList<>();
+
+  private boolean status;
+
+  private boolean captureStatus;
 
   /**
    * Initialize a camera object
    */
   public Camera() {
-    this.images = new HashMap<>();
+
   }
 
   /**
    * Returns the status of the Camera.
-   * @return
+   * @return true if camera is operational
    */
-  public int status() {
-    return 0;
+  public synchronized boolean status() {
+    return status;
+  }
+
+  @Override
+  public synchronized boolean captureStatus() {
+    return captureStatus;
   }
 
   /**
-   * Notifies us to take a picture with a zoom level (1-3) ??
-   * Returns a generated id
-   * @param zoom
-   * @return
+   * Notifies us to take a picture with a zoom level (0-3) ??
+   * @param zoom the zoom setting 0-3
    */
-  public int takePicture(int zoom) {
-    // Check to make sure we have enough storage space to save another picture. If not, then remove the oldest picture.
-    if (images.size() >= MAX_STORED_PICTURES) {
-      System.out.println("OUT OF MEMORY ERROR: removing oldest picture before taking another...");
-      int oldest_id = Collections.min(images.keySet());
-      images.remove(oldest_id);
-      System.out.format("Removed picture %d\n", oldest_id);
-    }
-
+  public void takePicture(int zoom) {
     // Take a new picture
-    Picture picture = generateImage(elapsed_seconds, zoom);
-    images.put(picture.getId(), picture);
-    return picture.getId();
+    image = generateImage(elapsed_seconds, zoom);
   }
 
-  public int takePicture() {
-    return takePicture(0);
+  public void takePicture() {
+    takePicture(0);
+  }
+
+  @Override
+  public Image getImageChunk(int x, int y, int size) {
+    return null;
+  }
+
+  @Override
+  public void on() {
+
+  }
+
+  @Override
+  public void off() {
+
+  }
+
+  @Override
+  public void reset() {
+
   }
 
   public void setElapsedSeconds(int elapsed_seconds) {
@@ -67,31 +78,28 @@ public class Camera implements Sensorable {
 
   /**
    * Returns a chunk of an image with the given id
-   * @param id
    * @param x
    * @param y
    * @param size
    * @return
    */
-  public Image imageChunk(int id, int x, int y, int size) {
-    Picture picture = images.get(id);
-    return picture.chunk(x, y, size);
+  public Image imageChunk(int x, int y, int size) {
+    return image.chunk(x, y, size);
   }
 
   /**
-   * Occasionally it may be necessary to get the whole picture object.
-   * @param id
-   * @return
+   * Used for simulation testing only
+   * @return entire image
    */
-  public Picture getPicture(int id) {
-    return images.get(id);
+  public Picture getPicture() {
+    return image;
   }
 
   /**
    * Generate a image at the given time with the given zoom
-   * @param time
-   * @param zoom
-   * @return
+   * @param time seconds since last picture
+   * @param zoom scale 0-3
+   * @return generated Picture object
    */
   private Picture generateImage(int time, int zoom) {
     BufferedImage image = new BufferedImage(image_size[0], image_size[1], BufferedImage.TYPE_INT_ARGB);
@@ -182,21 +190,5 @@ public class Camera implements Sensorable {
     g.drawString(Integer.toString(asteroid.getId()), asteroid.current_location[0], asteroid.current_location[1]);
   }
 
-  public static void main(String[] args) {
-    Camera camera = new Camera();
 
-    for (int i = 0; i < 10; i++) {
-      camera.setElapsedSeconds(i * 30);
-      int picture_id = camera.takePicture();
-      System.out.format("picture %d was taken\n", picture_id);
-      Picture picture = camera.getPicture(picture_id);
-      File output_file = new File("generated_image_" + picture.getId() + ".png");
-      try {
-        ImageIO.write(picture.get_image(), "png", output_file);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      System.out.format("Output picture %d to generated_image_%d.png\n", picture_id, picture_id);
-    }
-  }
 }
--- src/Sensorable.java
+++ src/Sensorable.java
@@ -2,42 +2,55 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 /**
+ *
  * Created by sean on 2/4/17.
  */
 public interface Sensorable {
   /**
    * Assesses and returns the status of the Camera
-   * @return int representing the status of the camera ??
+   * @return true if camera is operational
    */
-  public int status();
+  public boolean status();
+
+  /**
+   * Check image capture status
+   * @return true if capture of image is complete.
+   */
+  public boolean captureStatus();
 
   /**
    * Notifies camera to take a picture with a given zoom level (1-3) ??
    * @param zoom Level of zoom (1-3)
-   * @return Generated id of the picture
    */
-  public int takePicture(int zoom);
+  public void takePicture(int zoom);
 
   /**
    * Notified camera to take a picture with no zooming.
-   * @return Generated id of the picture
    */
-  public int takePicture();
+  public void takePicture();
 
   /**
-   * Generates an image at the given time with the given zoom level
-   * @param id
-   * @param x
-   * @param y
-   * @param size
-   * @return Image which is just a subsection of the larger picture
+   * Return square window from the image.
+   * @param x horizontal center of window
+   * @param y vertical center of window
+   * @param size width/height of window
+   * @return windowed image from full image.
    */
-  public Image imageChunk(int id, int x, int y, int size);
+  public Image getImageChunk(int x, int y, int size);
 
   /**
-   * Occasionally it is necessary to get the entire Picture object with a given id.
-   * @param id
-   * @return Picture object for the given id
+   * Attempt to turn camera on.
    */
-  public Picture getPicture(int id);
+  void on();
+
+  /**
+   * Attempt to turn camera off.
+   */
+  void off();
+
+  /**
+   * Reset camera
+   */
+  void reset();
+
 }
