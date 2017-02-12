4f8451f11de18fd73055ff54cb7f3568c9eb8b9e
Ally <star.goodman@gmail.com>
2017-02-12 15:16:55 -0700

    Added timeline for updating statuses

--- src/main/java/demo/DemoGUIController.java
+++ src/main/java/demo/DemoGUIController.java
@@ -65,7 +65,6 @@ public class DemoGUIController implements Initializable {
       public void handle(ActionEvent event) {
         updateCamStatusLabel();
         updateImageStatus();
-        System.out.println("Reached here in timeline");
       }
     }));
     updateClock.setCycleCount(Timeline.INDEFINITE);
