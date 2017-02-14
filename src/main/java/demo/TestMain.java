5cbc7b649ff54f0642599a921c098af6b64799a7
Sean Naegle <sean@naegles.com>
2017-02-14 10:57:55 -0700

    updated my test to test out zooming at each timestamp

--- src/main/java/demo/TestMain.java
+++ src/main/java/demo/TestMain.java
@@ -4,6 +4,7 @@ import sensor.Picture;
 import sensor.SensorSimulation;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,20 +13,47 @@ import java.io.IOException;
  */
 public class TestMain {
 
-    public static void main(String[] args) {
-        SensorSimulation camera = new SensorSimulation();
-        for (int i = 0; i < 10; i++) {
-            camera.setElapsedSeconds(i * 30);
-            camera.takePicture(0);
-            System.out.format("picture %d was taken\n", i);
-            Picture picture = camera.getPicture();
-            File output_file = new File("generated_image_" + i + ".png");
-            try {
-                ImageIO.write(picture.getImage(), "png", output_file);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.format("Output picture %d to generated_image_%d.png\n", i, i);
-        }
+  public static void main(String[] args) {
+    SensorSimulation camera = new SensorSimulation();
+    camera.on();
+    while(!camera.status()) {
+      System.out.println("Waiting for camera to turn on..." + camera.status());
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
+    for (int i = 0; i < 10; i++) {
+      camera.setElapsedSeconds(i * 30);
+      for (int zoom = 0; zoom < 4; zoom++) {
+        takePictureAndWriteToFile(camera, 0, i);
+      }
+    }
+  }
+
+  public static void takePictureAndWriteToFile(SensorSimulation camera, int zoom_level, int id) {
+    camera.takePicture(0);
+    while(!camera.captureStatus()) {
+      System.out.println("Picture not ready yet...");
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    System.out.format("picture %d was taken at zoom_level %d\n", id, zoom_level);
+    Picture picture = camera.getPicture();
+    File output_file = new File("generated_image_" + id + "_zoom_level_" + zoom_level + ".png");
+    writeImageToFile(picture.getImage(), output_file);
+    System.out.format("Output picture %d with zoom level %d to %s\n", id, zoom_level, output_file.getName());
+  }
+
+  public static void writeImageToFile(BufferedImage image, File output_file) {
+    try {
+      ImageIO.write(image, "png", output_file);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
