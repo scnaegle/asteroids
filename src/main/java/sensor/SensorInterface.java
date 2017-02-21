6197b553b3ec570726469cb03ba08e7c3d3b6ae8
Sean Naegle <sean@naegles.com>
2017-02-21 12:06:14 -0700

    updated the comments for the interface

--- src/main/java/sensor/SensorInterface.java
+++ src/main/java/sensor/SensorInterface.java
@@ -8,34 +8,36 @@ import java.awt.image.BufferedImage;
  */
 public interface SensorInterface {
   /**
-   * Assesses and returns the ready of the SensorSimulation
+   * Assesses the camera and returns true if the camera is on and operational
    * @return true if camera is operational
    */
   public boolean ready();
 
   /**
-   * Check imageView capture ready
-   * @return true if capture of imageView is complete.
+   * Check if image is ready to be grabbed and chunked.
+   * @return true if the image is ready
    */
   public boolean imageReady();
 
   /**
-   * Notifies camera to take a picture.
+   * Notifies camera to take a picture. This will start a thread for taking a picture. To determine
+   * if new picture is ready, use imageReady().
    */
   public void takePicture();
 
   /**
-   * Notifies the camera to set the zoom level as a multiplier
-   * @param zoom Level of zoom (0-16)
+   * Notifies the camera to set the zoom level, using one of the predefined levels defined by the
+   * ZoomLevel class.
+   * @param zoom ZoomLevel enum
    */
   public void setZoom(ZoomLevel zoom);
 
   /**
-   * Return square window from the imageView.
+   * Return square window of the image.
    * @param x horizontal center of window
    * @param y vertical center of window
    * @param size width/height of window
-   * @return windowed imageView from full imageView.
+   * @return image chunk using the defined (x,y) and size
    */
   public BufferedImage getImageChunk(int x, int y, int size);
 
